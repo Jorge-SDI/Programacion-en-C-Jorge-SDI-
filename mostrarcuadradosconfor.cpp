8b0798c580070fcf53a997b09195ce24e3e1b53f
Jorge Sdi <jorgesadi17@gmail.com>
2025-10-11 23:06:42 -0600

    Programa que retorna el cuadrado de un numero x

---
+++ mostrarcuadradosconfor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Ingrese un valor (N): " << endl;
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++){
+		cout << i * i << endl;
+	}
+	
+	
+	
+	return 0;
+}
+
