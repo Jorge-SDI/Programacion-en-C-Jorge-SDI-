0e6a3386f30054c49d4db1a7f1e500c666029232
Jorge Sdi <jorgesadi17@gmail.com>
2025-10-15 19:34:35 -0600

    Corregido el error de repeticion del promedio

--- promedio_mem_dinamica.cpp
+++ promedio_mem_dinamica.cpp
@@ -14,11 +14,21 @@ int main() {
 		cout << "Ingresa el numero [" << i << "] : ";
 		cin >> ptr[i];
 		suma += ptr[i];
-		
-		double promedio; 
-		promedio = suma / n;
-		cout << "El promedio es: " << promedio << endl;
 	}
+	
+	double promedio = static_cast<double>(suma) / n;
+	cout << "\nEl promedio es: " << promedio << endl;
+	
+	cout << "Valores del arreglo: [";
+	for (int i = 0; i < n; i++) {
+		cout << ptr[i];
+		if (i < n - 1) cout << ", "; 
+	}
+	cout << "]" << endl;
+	
+	
+	
+	
 	
 	delete[] ptr;
 	
